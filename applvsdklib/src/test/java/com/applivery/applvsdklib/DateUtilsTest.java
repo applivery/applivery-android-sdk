604e492f4036af8622f68e4505dacb3a94c7d89f
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-03-06 12:44:55 +0100

    Added expected exception

--- applvsdklib/src/test/java/com/applivery/applvsdklib/DateUtilsTest.java
+++ applvsdklib/src/test/java/com/applivery/applvsdklib/DateUtilsTest.java
@@ -45,7 +45,7 @@ public class DateUtilsTest {
         assertThat(expectedDate, not(isDateEqualTo(date)));
     }
 
-    @Test
+    @Test(expected = Exception.class)
     public void testStringToDateWithFormatWrongDate() throws Exception {
         String stringDate = "Valor Fake";
 
