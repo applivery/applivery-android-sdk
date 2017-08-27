84cfd72214cb6317a40c795b1cb4f0e4bfc2f56f
Manuel Rebollo <mrebollob@gmail.com>
2017-08-27 21:18:36 +0200

    Added applivery sdk to sample app

--- sample/src/main/java/com/applivery/sample/AppliveryApplication.kt
+++ sample/src/main/java/com/applivery/sample/AppliveryApplication.kt
@@ -1,7 +1,13 @@
 package com.applivery.sample
 
 import android.app.Application
-
+import com.applivery.applvsdklib.Applivery
 
 class AppliveryApplication : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+    Applivery.init(this, BuildConfig.APP_ID, BuildConfig.ACCOUNT_API_KEY, false)
+    Applivery.setUpdateCheckingInterval(0)
+  }
 }
