70c5639cff9a84eaba1b231b59323abbeba80765
Manuel Rebollo <mrebollob@gmail.com>
2018-11-03 13:06:14 +0100

    Move feedback setting to settings view

--- sample/src/main/java/com/applivery/sample/MainActivity.kt
+++ sample/src/main/java/com/applivery/sample/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.applivery.sample
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.widget.Chronometer
 import com.applivery.applvsdklib.Applivery
 import kotlinx.android.synthetic.main.activity_main.checkForUpdatesBackgroundSwitch
+import kotlinx.android.synthetic.main.activity_main.chronometer
+import kotlinx.android.synthetic.main.activity_main.feedbackSwitch
+import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,36 +14,30 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val toolbar = findViewById<Toolbar>(R.id.toolbar)
-    setSupportActionBar(toolbar)
-
-    val disable = findViewById<FloatingActionButton>(R.id.fab)
-    val enable = findViewById<FloatingActionButton>(R.id.fab2)
-
-    enable.setOnClickListener { view ->
-      Applivery.enableShakeFeedback()
-      Applivery.enableScreenshotFeedback()
-      Snackbar.make(view, "Feedback Enabled", Snackbar.LENGTH_LONG).show()
-    }
-
-    disable.setOnClickListener { view ->
-      Applivery.disableShakeFeedback()
-      Applivery.disableScreenshotFeedback()
-      Snackbar.make(view, "Feedback disabled", Snackbar.LENGTH_LONG).show()
-    }
 
     initViews()
   }
 
   private fun initViews() {
-    startChrono()
+    setSupportActionBar(toolbar)
+    chronometer.start()
+
     checkForUpdatesBackgroundSwitch.isChecked = Applivery.getCheckForUpdatesBackground()
     checkForUpdatesBackgroundSwitch.setOnCheckedChangeListener { _, isChecked ->
       Applivery.setCheckForUpdatesBackground(isChecked)
     }
-  }
 
-  private fun startChrono() {
-    (findViewById<Chronometer>(R.id.chronometer)).start()
+    Applivery.disableShakeFeedback()
+    Applivery.disableScreenshotFeedback()
+    feedbackSwitch.isChecked = false
+    feedbackSwitch.setOnCheckedChangeListener { _, isChecked ->
+      if (isChecked) {
+        Applivery.enableShakeFeedback()
+        Applivery.enableScreenshotFeedback()
+      } else {
+        Applivery.disableShakeFeedback()
+        Applivery.disableScreenshotFeedback()
+      }
+    }
   }
 }
