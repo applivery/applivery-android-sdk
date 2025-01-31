57d3a774edfa0d5c31f4984c66f5fa1b163ddd0a
Alvaro Blanco Cabrero <alvaro@applivery.com>
2025-01-31 15:13:33 +0100

    Revert back public module JAR integration in favor of module implementation and publication

--- sdk/public/build.gradle.kts
+++ sdk/public/build.gradle.kts
@@ -13,6 +13,9 @@ sdkPublish {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+
+    withSourcesJar()
+    withJavadocJar()
 }
 kotlin {
     compilerOptions {
