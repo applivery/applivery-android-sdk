5c4dbb869510e4ba464794808e46e34aff547c7e
Alvaro Blanco Cabrero <alvaro@applivery.com>
2025-06-10 18:31:23 +0200

    Bump SDK version to 4.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,5 +16,5 @@ val androidMinSdk by extra { 21 }
 val androidTargetSdk by extra { 35 }
 
 val libraryGroup by extra { "com.applivery" }
-val libraryVersion by extra { "4.4.2" }
+val libraryVersion by extra { "4.5.0" }
 
